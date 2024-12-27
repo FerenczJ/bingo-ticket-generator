faf62862d225460b1031fd3e4b3d4a5dd7eb1508
jozsef.ferencz <jozsef.ferencz@chess.com>
2024-12-27 11:49:22 +0100

    Initial project structure.

---
+++ src/main/java/com/mrq/bingo/BingoTicketGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mrq.bingo;
+
+/**
+ * Hello world!
+ *
+ */
+public class BingoTicketGeneratorApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
